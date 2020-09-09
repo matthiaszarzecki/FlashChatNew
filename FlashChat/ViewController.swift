456cafbffcd22adeb4414cfc2e8b5200aab8aa7b
Shruti <shruti.upadhya@gmail.com>
2020-09-09 13:45:06 +0200

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Shruti on 09/09/20.
+//  Copyright © 2020 Shruti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
